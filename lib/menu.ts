1abbbaeff24a9ca5b0042733b5e0ccff8c36e969
pacifiquerubasha <p.kishinyambwe@alustudent.com>
2024-06-08 14:56:44 +0400

    Repo setup

---
+++ lib/menu.ts
@@ -0,0 +1,113 @@
+import {
+    FileSpreadsheet,
+    FileText,
+    Users,
+    FolderKanban,
+    Calendar,
+    UserRoundCheck,
+    LayoutDashboard,
+    Building
+  } from "lucide-react";
+  
+  
+export type MenuItem = {
+    name: string,
+    url: string,
+    icon: any
+}
+
+type Menu = {
+    [key: string]: MenuItem[]
+}
+
+export const menu:Menu = {
+    "User" : [
+        {
+            "name": "Overview",
+            "url": "/overview",
+            icon: LayoutDashboard
+        },
+        {
+            "name": "Datasets",
+            "url": "/datasets",
+            icon: FileSpreadsheet
+        },
+        {
+            "name": "Articles",
+            "url": "/articles",
+            icon: FileText
+        },
+        {
+            "name": "Projects",
+            "url": "/projects",
+            icon: FolderKanban
+        },
+    ],      
+    "Institution" : [
+        {
+            "name": "Overview",
+            "url": "/overview",
+            icon: LayoutDashboard
+        },
+        {
+            "name": "Datasets",
+            "url": "/datasets",
+            icon: FileSpreadsheet
+        },
+        {
+            "name": "Articles",
+            "url": "/articles",
+            icon: FileText
+        },
+        {
+            "name": "Projects",
+            "url": "/projects",
+            icon: FolderKanban
+        },
+        {
+            "name": "Affiliated Users",
+            "url": "/affiliated-users",
+            icon: UserRoundCheck
+        },
+    ],
+    "Admin" : [
+        {
+            "name": "Overview",
+            "url": "/overview",
+            icon: LayoutDashboard
+        },
+        {
+            "name": "Datasets",
+            "url": "/datasets",
+            icon: FileSpreadsheet
+        },
+        {
+            "name": "Articles",
+            "url": "/articles",
+            icon: FileText
+        },
+        {
+            "name": "Projects",
+            "url": "/projects",
+            icon: FolderKanban
+        },
+        {
+            "name": "Events",
+            "url": "/events",
+            icon: Calendar
+        },
+        {
+            "name": "Users",
+            "url": "/users",
+            icon: Users
+        },
+        {
+            "name": "Institutions",
+            "url": "/institutions",
+            icon: Building
+        },
+        
+    ],
+    
+    
+}
---
+++ providers/MainProvider.tsx
@@ -0,0 +1,234 @@
+"use client";
+
+import * as React from "react";
+import Image from "next/image";
+import Link from "next/link";
+import {
+  Home,
+  LineChart,
+  Package,
+  Package2,
+  PanelLeft,
+  Search,
+  Settings,
+  ShoppingCart,
+  Truck,
+  Users2,
+} from "lucide-react";
+
+import {
+  Breadcrumb,
+  BreadcrumbItem,
+  BreadcrumbLink,
+  BreadcrumbList,
+  BreadcrumbPage,
+  BreadcrumbSeparator,
+} from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+
+import {
+  DropdownMenu,
+  DropdownMenuCheckboxItem,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
+import { Input } from "@/components/ui/input";
+
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipTrigger,
+  TooltipProvider,
+} from "@/components/ui/tooltip";
+
+import { menu, MenuItem } from "@/lib/menu";
+import { usePathname } from "next/navigation";
+
+type BreadcrumbItem = {
+  title: string;
+  href: string;
+};
+
+type Props = {
+  breadcrumbs: BreadcrumbItem[];
+  children: React.ReactNode;
+};
+
+export const MainProvider: React.FC<Props> = ({ breadcrumbs, children }) => {
+  const pathname = usePathname();
+  const [menuItems, setMenuItems] = React.useState<MenuItem[]>(menu["User"]);
+
+  return (
+    <TooltipProvider>
+      <div className="flex min-h-screen w-full flex-col bg-muted/40">
+        <aside className="fixed inset-y-0 left-0 z-10 hidden w-14 flex-col border-r bg-background sm:flex">
+          <nav className="flex flex-col items-center gap-4 px-2 sm:py-4">
+            <Link
+              href="#"
+              className="group flex h-9 w-9 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:h-8 md:w-8 md:text-base"
+            >
+              <Package2 className="h-4 w-4 transition-all group-hover:scale-110" />
+              <span className="sr-only">Acme Inc</span>
+            </Link>
+
+            {menuItems.map((item: MenuItem, index: number) => {
+              return (
+                <Tooltip key={index}>
+                  <TooltipTrigger asChild>
+                    <Link
+                      href={item.url}
+                      className={`flex h-9 w-9 items-center justify-center rounded-lg ${
+                        item.url === pathname
+                          ? "bg-accent text-accent-foreground"
+                          : "text-muted-foreground"
+                      }  transition-colors hover:text-foreground md:h-8 md:w-8`}
+                    >
+                      <item.icon />
+                      <span className="sr-only">{item.name}</span>
+                    </Link>
+                  </TooltipTrigger>
+                  <TooltipContent side="right">{item.name}</TooltipContent>
+                </Tooltip>
+              );
+            })}
+          </nav>
+          <nav className="mt-auto flex flex-col items-center gap-4 px-2 sm:py-4">
+            <Tooltip>
+              <TooltipTrigger asChild>
+                <Link
+                  href="#"
+                  className="flex h-9 w-9 items-center justify-center rounded-lg text-muted-foreground transition-colors hover:text-foreground md:h-8 md:w-8"
+                >
+                  <Settings className="h-5 w-5" />
+                  <span className="sr-only">Settings</span>
+                </Link>
+              </TooltipTrigger>
+              <TooltipContent side="right">Settings</TooltipContent>
+            </Tooltip>
+          </nav>
+        </aside>
+
+        <div className="flex flex-col sm:gap-4 sm:py-4 sm:pl-14">
+          <header className="sticky top-0 z-30 flex h-14 items-center gap-4 border-b bg-background px-4 sm:static sm:h-auto sm:border-0 sm:bg-transparent sm:px-6">
+            <Sheet>
+              <SheetTrigger asChild>
+                <Button size="icon" variant="outline" className="sm:hidden">
+                  <PanelLeft className="h-5 w-5" />
+                  <span className="sr-only">Toggle Menu</span>
+                </Button>
+              </SheetTrigger>
+              <SheetContent side="left" className="sm:max-w-xs">
+                <nav className="grid gap-6 text-lg font-medium">
+                  <Link
+                    href="#"
+                    className="group flex h-10 w-10 shrink-0 items-center justify-center gap-2 rounded-full bg-primary text-lg font-semibold text-primary-foreground md:text-base"
+                  >
+                    <Package2 className="h-5 w-5 transition-all group-hover:scale-110" />
+                    <span className="sr-only">Acme Inc</span>
+                  </Link>
+                  <Link
+                    href="#"
+                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                  >
+                    <Home className="h-5 w-5" />
+                    Dashboard
+                  </Link>
+                  <Link
+                    href="#"
+                    className="flex items-center gap-4 px-2.5 text-foreground"
+                  >
+                    <ShoppingCart className="h-5 w-5" />
+                    Orders
+                  </Link>
+                  <Link
+                    href="#"
+                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                  >
+                    <Package className="h-5 w-5" />
+                    Products
+                  </Link>
+                  <Link
+                    href="#"
+                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                  >
+                    <Users2 className="h-5 w-5" />
+                    Customers
+                  </Link>
+                  <Link
+                    href="#"
+                    className="flex items-center gap-4 px-2.5 text-muted-foreground hover:text-foreground"
+                  >
+                    <LineChart className="h-5 w-5" />
+                    Settings
+                  </Link>
+                </nav>
+              </SheetContent>
+            </Sheet>
+            <Breadcrumb className="hidden md:flex">
+              <BreadcrumbList>
+                {breadcrumbs.map((item, index) => {
+                  return (
+                    <>
+                      <BreadcrumbItem>
+                        <BreadcrumbLink
+                          asChild={index < breadcrumbs.length - 1}
+                        >
+                          <Link href={item.href || "#"}>{item.title}</Link>
+                        </BreadcrumbLink>
+                      </BreadcrumbItem>
+                      {index < breadcrumbs.length - 1 && (
+                        <BreadcrumbSeparator />
+                      )}
+                    </>
+                  );
+                })}
+              </BreadcrumbList>
+            </Breadcrumb>
+            <div className="relative ml-auto flex-1 md:grow-0">
+              <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
+              <Input
+                type="search"
+                placeholder="Search..."
+                className="w-full rounded-lg bg-background pl-8 md:w-[200px] lg:w-[320px]"
+              />
+            </div>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button
+                  variant="outline"
+                  size="icon"
+                  className="overflow-hidden rounded-full"
+                >
+                  <Image
+                    src="/girl.jpg"
+                    width={36}
+                    height={36}
+                    alt="Avatar"
+                    className="overflow-hidden rounded-full"
+                  />
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="end">
+                <DropdownMenuLabel>My Account</DropdownMenuLabel>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Settings</DropdownMenuItem>
+                <DropdownMenuItem>Support</DropdownMenuItem>
+                <DropdownMenuSeparator />
+                <DropdownMenuItem>Logout</DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </header>
+
+          {children}
+        </div>
+      </div>
+    </TooltipProvider>
+  );
+};
+
+export default MainProvider;
